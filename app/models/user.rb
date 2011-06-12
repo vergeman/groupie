52d89c54dfc98858586152cd3a78f4fcda4978b8
vergeman <alan.verga@gmail.com>
2011-06-12 00:55:11 -0400

    added test validation for :email attribute in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,4 +27,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :username
+
+  validates :username, :presence => true, :uniqueness => true
+
 end
