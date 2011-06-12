ca6c7c64e8d1483a7e32e447a938052d6ed4ad19
vergeman <alan.verga@gmail.com>
2011-06-12 00:00:01 -0400

    added username column to database and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110611215337) do
+ActiveRecord::Schema.define(:version => 20110612035901) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(:version => 20110611215337) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
