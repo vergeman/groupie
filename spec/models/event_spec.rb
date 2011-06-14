6c379c98ee50dcd4753eea4a981de08af7e5e7cf
vergeman <alan.verga@gmail.com>
2011-06-14 02:05:43 -0400

    added validation tests for Event and Participant models

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -2,17 +2,30 @@ require 'spec_helper'
 
 describe Event do
 
+  before (:each) do
+    @event = Factory(:event)
+  end
+
 
   describe "participants" do
-
-    before (:each) do
-      @event = Factory(:event)
-    end
 
     it "should have a participants method" do
       @event.should respond_to(:participants)
     end
 
+  end
+
+
+  describe "model validations" do
+
+    it "should require admin_id" do
+      @attr = { :admin_id => "" }
+      Event.new(@attr).should_not be_valid
+    end
 
   end
+
+
+
+
 end
--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -43,7 +43,7 @@ describe Participant do
   end
 
 
-  describe "votes" do
+  describe "votes attributes" do
 
     it "should have a 'votes_remaining' attribute" do
       @participant.should respond_to(:votes_remaining)
@@ -51,5 +51,27 @@ describe Participant do
 
   end
 
+
+  describe "validations" do
+    
+    before(:each) do
+      @attr = {:user_id => 1, :event_id => 1, :votes_remaining => 20}
+    end
+
+
+    it "should require a nonblank user_id" do
+      @user.participants.new(@attr.merge(:user_id => "")).should_not be_valid
+    end
+
+    it "should require a nonblank event_id" do
+      @user.participants.new(@attr.merge(:event_id => "")).should_not be_valid
+    end
+
+    it "should require a nonblank votes_remaining" do
+      @user.participants.new(@attr.merge(:votes_remaining => "")).should_not be_valid
+    end
+
+
+  end
 
 end
